0cd898148e5dde5842ba3f566181896bb1c12b25
valbub <lera-bubnova@yandex.ru>
2015-02-18 15:57:39 +0300

    Changes in function return

--- Task1.py
+++ Task1.py
@@ -5,18 +5,22 @@ class Integer(object):
         self.module = number
     def add (self, summand):
         if isinstance(summand, Integer):
-            print self.module + summand.module
+            new = Integer
+            new.module = self.module + summand.module
+            return new.module
         else:
-            print 'Error: different types.'
+            return 'Error: different types.'
 
 class Rational(object):
     def __init__(self, number):
         self.module = number
     def add(self, summand):
         if isinstance (summand, Rational):
-            print self.module + summand.module
+            new = Rational
+            new.module = self.module + summand.module
+            return new.module
         else:
-            print 'Error: different types.'
+            return 'Error: different types.'
 
 class Complex(object):
     def __init__(self, realpart=None, imagpart =None):
@@ -32,9 +36,9 @@ class Complex(object):
             new = Complex()
             new.real = self.real + summand.real
             new.imaginary = self.imaginary + summand.imaginary
-            print (new.real, new.imaginary)
+            return (new.real, new.imaginary)
          else:
-            print 'Error: different types.'
+            return 'Error: different types.'
 
 
 x1 = Complex(1, -4)
@@ -50,6 +54,6 @@ y3 = Integer(15)
 def sum(a, b):
    return a.add(b)
 
-sum (x1, y1)
-sum (x2, y2)
-sum (x3, y3)
+print sum (x1, y1)
+print sum (x2, y2)
+print sum (x3, y3)
