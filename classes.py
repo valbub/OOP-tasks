3c94a71a3c7771481e53ef2097ae784b74fcacf7
valbub <lera-bubnova@yandex.ru>
2015-03-13 18:14:58 +0300

    Quaternion class has been added

--- classes.py
+++ classes.py
@@ -2,28 +2,56 @@ __author__ = 'Valeria'
 
 
 class Number(object):
-    def add(self, summand):
-        if (self.imag <> 0) or (summand.imag <> 0):
-            new = Complex ()
-            new.real = self.real + summand.real
-            new.imag = self.imag + summand.imag
-            return new.real, new.imag
-        else:
-            new = Real ()
-            new.real = self.real + summand.real
-            return new.real
+    pass
 
 class Integer(Number):
     def __init__(self, real=0):
         self.real = real
-        self.imag = 0
+        self.imaginary = 0
+    def add(self, summand):
+        new = Real()
+        new.real = self.real + summand.real
+        return new
+    def __str__(self):
+        return '%s' % self.real
 
-class Real(Number):
+class Real(Integer):
     def __init__(self, real=0):
+        super(Real, self).__init__(real)
         self.real = real
-        self.imag = 0
 
-class Complex(Number):
+class Complex(Real):
     def __init__(self, real=0, imag=0):
-        self.real = real
-        self.imag = imag
+        super(Complex, self).__init__(real)
+        self.imaginary = imag
+    def add(self, summand):
+        new = Complex()
+        new.real = self.real + summand.real
+        try:
+            new.imaginary = self.imaginary + summand.imag
+        except AttributeError:
+            new.imaginary = self.imaginary
+        return new
+    def __str__(self):
+      return '%s %s' % (self.real, self.imaginary)
+
+class Quaternion(Complex):
+    def __init__(self, real=0, imag=0, jcomp=0, kcomp=0):
+        super(Quaternion, self).__init__(real, imag)
+        self.jcomponent = jcomp
+        self.kcomponent = kcomp
+    def add(self, summand):
+        new = Quaternion()
+        new.real = self.real + summand.real
+        new.imaginary = self.imaginary + summand.imaginary
+        try:
+            new.jcomponent = self.jcomponent + summand.jcomponent
+        except AttributeError:
+            new.jcomponent = self.jcomponent
+        try:
+            new.kcomponent = self.kcomponent + summand.kcomponent
+        except AttributeError:
+            new.kcomponent = self.kcomponent
+        return new
+    def __str__(self):
+      return '%s %si %sj %sk' % (self.real, self.imaginary, self.jcomponent, self.kcomponent)
