942505fd29c5d69e3a8760c76c19391ca77a0768
valbub <lera-bubnova@yandex.ru>
2015-03-13 18:14:12 +0300

    Quaternions have been added

--- app.py
+++ app.py
@@ -4,8 +4,13 @@ from classes import *
 
 ex1 = Complex(1.15, 6)
 ex2 = Real(2.67)
+ex3 = Real(1.15)
+ex4 = Quaternion(2, 5.32, 5, 8.98)
+ex5 = Quaternion(24, 62, 3, 8)
 
 def sum(a, b):
    return a.add(b)
 
-print sum(ex1, ex2)
+res = sum(ex4, ex5)
+
+print res, type(res)
