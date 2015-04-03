961a1aa0d3da6b6bc74f0086f9fcaa87d0f0f73c
valbub <lera-bubnova@yandex.ru>
2015-04-03 13:54:17 +0300

    Optimised str method

--- classes.py
+++ classes.py
@@ -2,9 +2,14 @@ __author__ = 'Valeria'
 
 
 class Number(object):
-    pass
+    def __str__(self):
+#        return '%s'% self.__dict__
+        a = ''
+        for i in range(len(self.__dict__.keys())):
+            a += str(self.__dict__[(self.__dict__.keys())[i]]) + ' '
+        return a
 
-class Integer(Number):
+class Real(Number):
     def __init__(self, real=0):
         self.real = real
         self.imaginary = 0
@@ -12,13 +17,6 @@ class Integer(Number):
         new = Real()
         new.real = self.real + summand.real
         return new
-    def __str__(self):
-        return '%s' % self.real
-
-class Real(Integer):
-    def __init__(self, real=0):
-        super(Real, self).__init__(real)
-        self.real = real
 
 class Complex(Real):
     def __init__(self, real=0, imag=0):
@@ -32,8 +30,6 @@ class Complex(Real):
         except AttributeError:
             new.imaginary = self.imaginary
         return new
-    def __str__(self):
-      return '%s %s' % (self.real, self.imaginary)
 
 class Quaternion(Complex):
     def __init__(self, real=0, imag=0, jcomp=0, kcomp=0):
@@ -53,5 +49,3 @@ class Quaternion(Complex):
         except AttributeError:
             new.kcomponent = self.kcomponent
         return new
-    def __str__(self):
-      return '%s %si %sj %sk' % (self.real, self.imaginary, self.jcomponent, self.kcomponent)
