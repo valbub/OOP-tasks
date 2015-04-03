7dbea17c13eadd29d72687f52cf1473ea3b32bf9
valbub <lera-bubnova@yandex.ru>
2015-04-03 15:14:36 +0300

    Changes for str method

--- classes.py
+++ classes.py
@@ -6,13 +6,16 @@ class Number(object):
 #        return '%s'% self.__dict__
         a = ''
         for i in range(len(self.__dict__.keys())):
-            a += str(self.__dict__[(self.__dict__.keys())[i]]) + ' '
+            if self.__dict__.keys()[i] == 'real':
+                a += str(self.__dict__[(self.__dict__.keys())[i]]) + ' '
+            else:
+                a += str(self.__dict__[(self.__dict__.keys())[i]]) + str(self.__dict__.keys()[i]) + ' '
         return a
 
 class Real(Number):
     def __init__(self, real=0):
         self.real = real
-        self.imaginary = 0
+        self.i = 0
     def add(self, summand):
         new = Real()
         new.real = self.real + summand.real
@@ -21,31 +24,31 @@ class Real(Number):
 class Complex(Real):
     def __init__(self, real=0, imag=0):
         super(Complex, self).__init__(real)
-        self.imaginary = imag
+        self.i = imag
     def add(self, summand):
         new = Complex()
         new.real = self.real + summand.real
         try:
-            new.imaginary = self.imaginary + summand.imag
+            new.i = self.i + summand.i
         except AttributeError:
-            new.imaginary = self.imaginary
+            new.i = self.i
         return new
 
 class Quaternion(Complex):
     def __init__(self, real=0, imag=0, jcomp=0, kcomp=0):
         super(Quaternion, self).__init__(real, imag)
-        self.jcomponent = jcomp
-        self.kcomponent = kcomp
+        self.j = jcomp
+        self.k = kcomp
     def add(self, summand):
         new = Quaternion()
         new.real = self.real + summand.real
-        new.imaginary = self.imaginary + summand.imaginary
+        new.i = self.i + summand.i
         try:
-            new.jcomponent = self.jcomponent + summand.jcomponent
+            new.j = self.j + summand.j
         except AttributeError:
-            new.jcomponent = self.jcomponent
+            new.j = self.j
         try:
-            new.kcomponent = self.kcomponent + summand.kcomponent
+            new.k = self.k + summand.k
         except AttributeError:
-            new.kcomponent = self.kcomponent
+            new.k = self.k
         return new
